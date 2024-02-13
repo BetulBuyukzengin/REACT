8fbdaba1f3f046ba08b8542e51809662fb594e52
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-02-13 15:45:19 +0300

    Mevcut konumu getiren ve gelen konuma tıklandığında map içeren bir siteye yönlendirme mini projesi yaptım .

---
+++ 18-use-geolocation/src/App.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { isLoading, getPosition, error, position };
+}
+
+export default function App() {
+  const {
+    isLoading,
+    getPosition,
+    error,
+    position: { lat, lng },
+  } = useGeolocation();
+  const [countClicks, setCountClicks] = useState(0);
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+  return (
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
