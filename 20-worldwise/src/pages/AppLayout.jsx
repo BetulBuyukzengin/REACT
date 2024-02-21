108b06712e874ecfc0880acb93e77e4d51a46590
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-02-21 22:05:25 +0300

    fix: app layout file fixed

--- 20-worldwise/src/pages/AppLayout.jsx
+++ 20-worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>app</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
