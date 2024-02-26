e5b62e2e1e8d38ad58887cdb6474d5e625c773a4
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-02-26 15:35:17 +0300

    feat: placed map using leaflet lib

--- 20-worldwise/src/components/Map.jsx
+++ 20-worldwise/src/components/Map.jsx
@@ -1,23 +1,43 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
+
 function Map() {
   // programmatic navigation
   const navigate = useNavigate();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   // read location from URL
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  // const lat = searchParams.get("lat");
+  // const lng = searchParams.get("lng");
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position:{lat},{lng}
-      </h1>
-      {/* updates the query string */}
-      <button onClick={() => setSearchParams({ lat: 22, lng: 45 })}>
-        Change Pos
-      </button>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        // scrolla zoom ekleme
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
