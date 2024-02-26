47c207441202f591b540373a50b71a67e0c183cd
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-02-26 18:56:52 +0300

    feat: setting map position with geolocation and lat and lng values ​​clicked on the map are shown in the url

--- 20-worldwise/src/components/Map.jsx
+++ 20-worldwise/src/components/Map.jsx
@@ -1,18 +1,32 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
 import { useCities } from "../contexts/CitiesContext";
 import { useEffect, useState } from "react";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 function Map() {
-  // programmatic navigation
-  const navigate = useNavigate();
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   // read location from URL
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
+
   //! Seçilen şehrin konumu gösterildikten sonra bir önceki sayfaya geçmek için butona tıkladığımızda önceki pozisyon haritada aynı kalacak:
   //!  mapLat ve mapLng u senkronize tutuyor:
   useEffect(
@@ -21,8 +35,22 @@ function Map() {
     },
     [mapLat, mapLng]
   );
+
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={8}
@@ -45,15 +73,27 @@ function Map() {
           </Marker>
         ))}
         <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
+// center updated location
 // eslint-disable-next-line react/prop-types
 function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
-
   return null;
+}
+
+function DetectClick() {
+  // programmatic navigation
+  const navigate = useNavigate();
+  // look at the leaflet
+  useMapEvents({
+    // eslint-disable-next-line no-irregular-whitespace
+    //! Latitude and longitude values ​​clicked on the map are shown in the url
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 export default Map;
