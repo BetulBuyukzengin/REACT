46ad71b0af0e4027368b6dd312b1466af668aff6
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-02-29 19:12:21 +0300

    feat:auth context created and used reducer

--- 20-worldwise/src/App.jsx
+++ 20-worldwise/src/App.jsx
@@ -11,35 +11,38 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContex";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            {/* <Route
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              {/* <Route
             index
             element={<CityList cities={cities} isLoading={isLoading} />}
           /> */}
-            {/* Gezinmek veya URL ye yönlendirme - replace, geri gitmek için */}
-            <Route index replace element={<Navigate />} />
-            {/* Child Routes */}
-            <Route path="cities" element={<CityList />} />
-            {/* The URL for state management */}
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          {/* URL diğer route lar * eşleşmiyor ise bu PageNotFound sayfası görünecek.  */}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+              {/* Gezinmek veya URL ye yönlendirme - replace, geri gitmek için */}
+              <Route index replace element={<Navigate />} />
+              {/* Child Routes */}
+              <Route path="cities" element={<CityList />} />
+              {/* The URL for state management */}
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            {/* URL diğer route lar * eşleşmiyor ise bu PageNotFound sayfası görünecek.  */}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
