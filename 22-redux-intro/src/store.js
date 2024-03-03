c6ca3a28c2a2239f3b74fc067c546292b70ae8e3
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-03 17:44:51 +0300

    feat: learn redux ,create redux store

---
+++ 22-redux-intro/src/store.js
@@ -0,0 +1,46 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    //! Reducer da olduğu gibi error oluşturmuyoruz, olmadığı durumda da state güncellenmeyecek
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
