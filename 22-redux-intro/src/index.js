8b25a0eb5459dba9f53116c9588465a2eaf5a5b0
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-07 13:53:57 +0300

    feat: connecting our redux app with react

--- 22-redux-intro/src/index.js
+++ 22-redux-intro/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./store";
+import store from "./store";
+import { Provider } from "react-redux"; //!React store ve  uygulamasını bağlamak için
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
