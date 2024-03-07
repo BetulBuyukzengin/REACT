f83506475b5502d2a0fa50ae44a5489be5aa1459
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-07 14:35:32 +0300

    feat: redux file structure, state slices

---
+++ 22-redux-intro/src/store-v1.js
@@ -0,0 +1,116 @@
+import { combineReducers, createStore } from "redux";
+
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    //! Reducer da olduğu gibi error oluşturmuyoruz, olmadığı durumda da state güncellenmeyecek
+    default:
+      return state;
+  }
+}
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+//! Reducerları birleştirmek için route reducer oluşturup combineReducers fonksiyonunu çağırma
+const routeReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+//! Store oluşturma
+// const store = createStore(accountReducer);
+const store = createStore(routeReducer);
+
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+//const ACCOUNT_DEPOSIT="account/deposit"; --> eski react tabanlı projelerde, değişkenlerde tutulurdu
+//! Action creator functions
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(100));
+store.dispatch(requestLoan(1000, "Buy a car"));
+console.log(store.getState());
+store.dispatch(payLoan());
+console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+store.dispatch(createCustomer("Betül B.", "12135"));
+store.dispatch(deposit(100));
+console.log(store.getState());
+store.dispatch(updateName("Beyza"));
+console.log(store.getState());
+
+export default store;
