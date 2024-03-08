41391746e2e02256ad89de65ab5e1b89f21fc701
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-08 16:39:05 +0300

    feat: add currency conversion

--- 22-redux-intro/src/features/accounts/accountSlice.js
+++ 22-redux-intro/src/features/accounts/accountSlice.js
@@ -14,6 +14,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -46,10 +47,32 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = "";
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 export default accountSlice.reducer;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  // for thunk middleware
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    //Api call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    console.log(data);
+    const converted = data.rates.USD;
+    console.log(converted);
+    //return action
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
 // export default function accountReducer(state = initialState, action) {
 //   switch (action.type) {
 //     case "account/deposit":
